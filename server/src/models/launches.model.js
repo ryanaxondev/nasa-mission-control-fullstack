3a58732608c905faf4f129fa331e38e2a1c014dd
ryanaxondev <ryanaxondev@gmail.com>
2026-02-11 00:21:53 +0330

    fix(launches-model): fix launchDate property typo
    Correct misspelled 'launcheDate' property to 'launchDate' to match project schema and prevent data inconsistency.

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,7 +4,7 @@ const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
-    launcheDate : new Date('December 27, 2030'),
+    launchDate : new Date('December 27, 2030'),
     destination : 'Kepler-442 b',
     customer : ['AXON', 'NASA'],
     upcoming : true,
